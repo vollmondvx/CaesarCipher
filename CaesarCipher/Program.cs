8667fcfa78059e7273f10c2e9157052687e06fc8
VollmondVX <234042768+vollmondvx@users.noreply.github.com>
2025-10-02 23:04:31 +0200

    Solved bug of negative keys.

--- CaesarCipher/Program.cs
+++ CaesarCipher/Program.cs
@@ -1,23 +1,28 @@
-﻿string msg = "HELLO";
+﻿using System.Drawing;
+
+string msg = "HELLO,";
 int key = 20;
-
-
-
-Console.WriteLine(Encrypt(msg, key));
 
 static string Encrypt(string msg, int key)
 {
     char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
     int size = letters.Length;
-    Console.WriteLine(size);
     string encrypted = "";
 
     foreach (char letter in msg)
     {
         int index = Array.IndexOf(letters, letter);
+        if (index == -1)
+        {
+            encrypted += letter;
+            continue;
+        }
 
-        int newIndex = (index + key) > 26 ? index + key - size : index + key;
+        int newIndex = (index + key) % size;
         encrypted += letters[newIndex];
     }
     return encrypted;
 }
+string encrypted = Encrypt(msg, key);
+Console.WriteLine(encrypted);
+Console.WriteLine(Encrypt(encrypted, 26 - key));
