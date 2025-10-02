d5250e774b1fa72bea927d624278f49c313803c8
VollmondVX <234042768+vollmondvx@users.noreply.github.com>
2025-10-02 21:44:55 +0200

    First commit

---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+string msg = "HELLO";
+int key = 20;
+
+
+
+Console.WriteLine(Encrypt(msg, key));
+
+static string Encrypt(string msg, int key)
+{
+    char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+    int size = letters.Length;
+    Console.WriteLine(size);
+    string encrypted = "";
+
+    foreach (char letter in msg)
+    {
+        int index = Array.IndexOf(letters, letter);
+
+        int newIndex = (index + key) > 26 ? index + key - size : index + key;
+        encrypted += letters[newIndex];
+    }
+    return encrypted;
+}
